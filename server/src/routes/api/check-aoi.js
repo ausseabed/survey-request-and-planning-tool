eb2c61835c19c3a91bdd5452dae222ec5004803f
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-01-09 13:47:08 +1100

    implemented check area of interest intersection handler

--- server/src/routes/api/check-aoi.js
+++ server/src/routes/api/check-aoi.js
@@ -4,17 +4,32 @@ const boom = require('boom');
 
 import { getConnection } from 'typeorm';
 
-import { asyncMiddleware, isAuthenticated } from '../utils';
+import { asyncMiddleware, isAuthenticated, geojsonToMultiPolygon }
+  from '../utils';
+import { ProjectMetadata } from '../../lib/entity/project-metadata';
 
 var router = express.Router();
 
+router.post('/', isAuthenticated, asyncMiddleware(async function (req, res) {
+  // Accepts a geojson object (assumed already in epsg:4326) and returns
+  // a list of all projects that have areaOfInterests that intersect this
+  // geojson feature.
+  const geojson = geojsonToMultiPolygon(req.body);
+  const geomString = JSON.stringify(geojson.geometry);
 
-router.post('/', isAuthenticated, function (req, res) {
-  console.log(req.body);
+  let projects = await getConnection()
+  .getRepository(ProjectMetadata)
+  .createQueryBuilder("project_metadata")
+  .where(
+    `ST_Intersects(
+      project_metadata.areaOfInterest,
+      ST_SetSRID(ST_GeomFromGeoJSON(:geomStr),4326)
+    )`,
+    {geomStr: geomString}
+  )
+  .getMany();
 
-  return res.json({
-    'matching': []
-  });
-})
+  return res.json(projects);
+}))
 
 module.exports = router;

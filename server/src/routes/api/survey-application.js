2935dbb719e588abd7aeab30b787376ae05b307a
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-01-14 13:40:39 +1100

    survey application handler will now provide a list of unique group names

--- server/src/routes/api/survey-application.js
+++ server/src/routes/api/survey-application.js
@@ -10,6 +10,15 @@ import { SurveyApplication } from '../../lib/entity/survey-application';
 
 var router = express.Router();
 
+router.get('/group-names', async function (req, res) {
+  let grpNames = await getConnection()
+  .getRepository(SurveyApplication)
+  .createQueryBuilder("survey_application")
+  .select('DISTINCT ON (survey_application.group) survey_application.group as group').orderBy("survey_application.group", "ASC").getRawMany();
+
+  return res.json(grpNames);
+});
+
 // Gets a list of survey applications
 router.get('/', async function (req, res) {
   let surveyApps = await getConnection().getRepository(SurveyApplication)

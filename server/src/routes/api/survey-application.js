06e227403a4adc3468d29545064188b782d5af94
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-01-14 15:05:15 +1100

    return group names as simple list of strings (not objects)

--- server/src/routes/api/survey-application.js
+++ server/src/routes/api/survey-application.js
@@ -13,12 +13,16 @@ var router = express.Router();
 router.get('/group-names', async function (req, res) {
   // get a list of the unique group names for survey applications. This supports
   // the two stage selection process of survey applications
-  let grpNames = await getConnection()
+  let grps = await getConnection()
   .getRepository(SurveyApplication)
   .createQueryBuilder("survey_application")
   .select(
     'DISTINCT ON (survey_application.group) survey_application.group as group')
   .orderBy("survey_application.group", "ASC").getRawMany();
+
+  let grpNames = grps.map(grp => {
+    return grp.group;
+  })
 
   return res.json(grpNames);
 });

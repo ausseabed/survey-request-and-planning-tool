0e272b8447172af752091e7437a776d1f724c346
Alex Leith <alexgleith@gmail.com>
2019-01-15 11:10:04 +1100

    add slash

--- pipeline/build-web.sh
+++ pipeline/build-web.sh
@@ -3,5 +3,5 @@ set -xe
 docker build \
     -t crcsi/qa4mbes-www:$IMAGE_TAG \
     --build-arg AUTH_HOST=$AUTH_HOST \
-    --build-arg AUTH_CLIENT_ID=$AUTH_CLIENT_ID
+    --build-arg AUTH_CLIENT_ID=$AUTH_CLIENT_ID \
     client

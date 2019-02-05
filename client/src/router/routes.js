eee026c9da01e8636e132462f724c2a92ed8d5c6
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-02-05 21:10:47 +1100

    cleanup; removing old UAV code

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,11 +1,6 @@
 import Login from 'components/login.vue'
 import Main from 'components/Main.vue'
 
-import UavHome from 'components/uav/Home.vue'
-import UavSurvey from 'components/uav/Survey.vue'
-import UavTender from 'components/uav/Tender.vue'
-import UavSession from 'components/uav/Session.vue'
-import UavProjects from 'components/uav/Projects.vue'
 import ProjectMetadata from 'components/uav/ProjectMetadata.vue'
 import SurveyTechnicalSpecification
   from 'components/uav/survey-technical-specification.vue'
@@ -21,14 +16,6 @@ export default [
     { path: '/', component: Main },
     { path: '/login', component: Login },
 
-    { path: '/customdataset/:id', name: 'CreateCustomDataset', component: CustomDataset },
-    { path: '/standarddataset/:id', name: 'StandardDataset', component: StandardDataset },
-
-    { path: '/uav', component: UavHome },
-    { path: '/uav/survey', component: UavSurvey },
-    { path: '/uav/tender', component: UavTender },
-    { path: '/uav/session', component: UavSession },
-    { path: '/uav/project', component: UavProjects },
     {
       path: '/survey/new',
       component: ProjectMetadata,

c04b7df689bb110b5a0d266e907c807d83e2a96f
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-02-07 15:16:33 +1100

    include new data to capture type in most recent migration script

--- server/src/migration/1549429487016-new-defaults.ts
+++ server/src/migration/1549429487016-new-defaults.ts
@@ -17,6 +17,21 @@ export class newDefaults1549429487016 implements MigrationInterface {
 
         await updateSurveyApplications(
           queryRunner, surveyApplications);
+
+        // seed database with default data capture types
+        const stdDataCaptureTypes= ['Multibeam data'];
+        for (const dctName of stdDataCaptureTypes) {
+          await queryRunner
+           .manager
+           .createQueryBuilder()
+           .insert()
+           .into("data_capture_type")
+           .values({
+             name:dctName,
+             userSubmitted: false
+           })
+           .execute();
+        };
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {

1352ab95865cf205db260103439c6d9c97048720
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-03-28 22:03:36 +1100

    only request _non_ user submitted survey applications

--- client/src/store/modules/survey-application/survey-application-actions.js
+++ client/src/store/modules/survey-application/survey-application-actions.js
@@ -4,7 +4,10 @@ import { EventBus } from './../../../event-bus';
 export const getSurveyApplications = ({ commit, state }) => {
   var url_endpoint = '/api/survey-application/';
   const payload = state.selectedSurveyApplicationGroup ?
-    {params:{group: state.selectedSurveyApplicationGroup}} :
+    {params:{
+      group: state.selectedSurveyApplicationGroup,
+      "user-submitted":false
+    }} :
     {}
 
   return new Promise((resolve, reject) => {
@@ -21,9 +24,14 @@ export const getSurveyApplications = ({ commit, state }) => {
 
 export const getSurveyApplicationGroups = ({ commit, state }) => {
   var url_endpoint = '/api/survey-application/group-names';
+  const payload = {
+    params:{
+      "user-submitted":false
+    }
+  }
 
   return new Promise((resolve, reject) => {
-    Vue.axios.get(url_endpoint)
+    Vue.axios.get(url_endpoint, payload)
     .then((response) => {
       commit('setSurveyApplicationGroups', response.data);
       resolve(response.data);

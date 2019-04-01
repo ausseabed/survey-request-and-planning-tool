4092bbd52914419e33cbd79c17ce91449e1f741c
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-04-02 10:39:11 +1100

    updated users skeleton with that from orgs before things get too complicated

--- client/src/components/users/users.vue
+++ client/src/components/users/users.vue
@@ -1,16 +1,35 @@
 <template>
 
-  <div class="q-mt-md">
-    <q-card>
-      <q-card-title>
-        Users
-      </q-card-title>
-      <q-card-separator />
-      <q-card-main>
-        Not implemented.
-      </q-card-main>
-    </q-card>
-  </div>
+    <div class="q-my-md fit">
+      <div class="fit">
+        <div class="row gutter-sm fit content-stretch">
+          <div class="col-sm-12 ol-lg-12 col-xl-6 self-stretch ">
+            <q-card class="fit">
+              <q-card-title>
+                Users
+              </q-card-title>
+              <q-card-separator />
+              <q-card-main>
+                Not implemented.
+              </q-card-main>
+            </q-card>
+          </div>
+          <div class="col-sm-12 col-lg-12 col-xl-6 self-start">
+            <q-card class="fit">
+              <q-card-title>
+                User
+              </q-card-title>
+              <q-card-separator />
+              <q-card-main>
+                Not implemented.
+              </q-card-main>
+            </q-card>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
 </template>
 
 <script>

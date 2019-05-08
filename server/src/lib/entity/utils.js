67c9fffc3276ec8fc6fc7bde0e75910bf1f44b03
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-05-08 17:00:03 +1000

    bug fix; date transformer wasn't handling undefined values
    was producing an invalid date instead of undefined when passed undefined. This was causing issues when trying to save the invalid date to the database

--- server/src/lib/entity/utils.js
+++ server/src/lib/entity/utils.js
@@ -1,5 +1,10 @@
+import _ from 'lodash'
+
 export class DateTransformer {
   to (value) {
+    if (_.isNil(value)) {
+      return undefined
+    }
     const date = new Date();
     date.setTime(value);
     return date;

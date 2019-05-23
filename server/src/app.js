b379c346bcdea6176d6235ac7faaa61c1306171f
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-05-23 11:53:25 +1000

    increase request body size to 50mb
    for when large sets of geometry is attached to a project/hipp request

--- server/src/app.js
+++ server/src/app.js
@@ -29,7 +29,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));

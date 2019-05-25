7fae2ac680c264f846af9acb8e4e6caec3ea46c6
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-05-25 14:48:20 +1000

    added http request interceptor. Redirects user to login page if an axois request returns with a 401

--- client/src/boot/axios.js
+++ client/src/boot/axios.js
@@ -1,7 +1,19 @@
 import axios from 'axios'
 import VueAxios from 'vue-axios';
 
-export default ({ Vue }) => {
+export default ({ app, router, Vue, store }) => {
+  axios.interceptors.response.use(
+    function (response) {
+      return response
+    },
+    function (error) {
+      if (error.response.status === 401) {
+        router.app.$auth.logout()
+        router.push({ path: `/login` })
+      }
+      return Promise.reject(error)
+    }
+  );
   Vue.use(VueAxios, axios);
   // Todo: see if the baseURL can be dynamic...
   // axios.defaults.baseURL = process.env.API_HOST;

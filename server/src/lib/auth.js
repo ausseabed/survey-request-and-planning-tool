27bde3e0e3d557925ea69bf4b8e24a2014ed1dda
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-05-27 13:19:00 +1000

    formatting; indentation changed to two spaces

--- server/src/lib/auth.js
+++ server/src/lib/auth.js
@@ -3,22 +3,22 @@ var fs = require('fs');
 var resolve = require('path').resolve;
 
 var auth = function () {
-    return (function () {
-        return {
-            verify: function (token) {
-                // Verifies if the bearer token is valid
-                var cert_priv = fs.readFileSync(resolve(__dirname + './../../ssh_keys/public'));
-                var verified = false;
-                // slice to remove "Bearer " from the token
-                jwt.verify(token.slice(7), cert_priv, { algorithms: ['RS256'] }, function (err, decoded) {
-                    if (decoded) {
-                        verified = decoded;
-                    }
-                });
-                return verified;
-            }
-        };
-    }());
+  return (function () {
+    return {
+      verify: function (token) {
+        // Verifies if the bearer token is valid
+        var cert_priv = fs.readFileSync(resolve(__dirname + './../../ssh_keys/public'));
+        var verified = false;
+        // slice to remove "Bearer " from the token
+        jwt.verify(token.slice(7), cert_priv, { algorithms: ['RS256'] }, function (err, decoded) {
+          if (decoded) {
+            verified = decoded;
+          }
+        });
+        return verified;
+      }
+    };
+  }());
 };
 
 module.exports = auth;

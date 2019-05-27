bd6b83a9e4f24741666424700b7cf00e76a8879f
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-05-27 14:04:33 +1000

    add a restricted basic user role; can only do stuff linked to the org they are assigned

---
+++ server/src/migration/1558929196468-add-role-basic-restricted.ts
@@ -0,0 +1,27 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+import { Role } from '../lib/entity/role'
+
+export class addRoleBasicRestricted1558929196468 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+      let basicResRole = new Role();
+      basicResRole.name = "Basic - restricted to organisation";
+      basicResRole.canViewOrgProjects = true;
+      basicResRole.canViewOrgHippRequests = true;
+
+      basicResRole = await queryRunner.manager
+      .getRepository(Role)
+      .save(basicResRole);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+      await queryRunner.manager
+      .createQueryBuilder()
+      .delete()
+      .from(Role)
+      .where("name = :name", { name: "Basic - restricted to organisation" })
+      .execute();
+    }
+
+}

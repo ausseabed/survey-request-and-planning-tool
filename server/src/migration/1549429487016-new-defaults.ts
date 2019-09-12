36f72787fc02e992a0b60df150680ff3ee03f608
Lachlan Hurst <lachlan.hurst@gmail.com>
2019-09-12 14:17:27 +1000

    fix broken migration
    entity definition was more advanced than the database at migration point

--- server/src/migration/1549429487016-new-defaults.ts
+++ server/src/migration/1549429487016-new-defaults.ts
@@ -4,20 +4,6 @@ export class newDefaults1549429487016 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
 
-        // seed database with default data capture types
-        const stdDataCaptureTypes= ['Multibeam data'];
-        for (const dctName of stdDataCaptureTypes) {
-          await queryRunner
-           .manager
-           .createQueryBuilder()
-           .insert()
-           .into("data_capture_type")
-           .values({
-             name:dctName,
-             userSubmitted: false
-           })
-           .execute();
-        };
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
--- server/src/migration/1562739183953-applies-to-plansorrequests-for-datacapturetypes.ts
+++ server/src/migration/1562739183953-applies-to-plansorrequests-for-datacapturetypes.ts
@@ -10,6 +10,21 @@ export class appliesToPlansorrequestsForDatacapturetypes1562739183953 implements
         await queryRunner.query(`ALTER TABLE "data_capture_type" ADD "appliesToRequest" boolean NOT NULL DEFAULT false`);
 
         await insertDataAndInstrumentTypes(queryRunner, dataAndInstrumentTypes);
+
+        // seed database with default data capture types
+        const stdDataCaptureTypes= ['Multibeam data'];
+        for (const dctName of stdDataCaptureTypes) {
+          await queryRunner
+           .manager
+           .createQueryBuilder()
+           .insert()
+           .into("data_capture_type")
+           .values({
+             name:dctName,
+             userSubmitted: false
+           })
+           .execute();
+        };
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {

3f3c8b1fb417ab76405c9d55928df955a5b9b24f
Lachlan Hurst <lachlan.hurst@gmail.com>
2020-04-15 14:43:58 +1000

    uploaded data now saved in task by handler, but nothing done with data yet

--- server/src/routes/api/priority-area.js
+++ server/src/routes/api/priority-area.js
@@ -10,6 +10,7 @@ import { getConnection } from 'typeorm';
 
 import { asyncMiddleware, isAuthenticated } from '../utils';
 import { PriorityArea } from '../../lib/entity/priority-area';
+import { Task } from '../../lib/entity/task';
 
 const router = express.Router();
 
@@ -21,6 +22,44 @@ router.put(
   asyncMiddleware(async function (req, res)
 {
   console.log("upload");
+
+  let task = new Task();
+  let fields = {};
+
+  new formidable.IncomingForm().parse(req)
+    .on('field', (name, field) => {
+      fields[name] = field;
+    })
+    .on('file', async (name, file) => {
+      task.blobFileName = file.name;
+      const data = fs.readFileSync(file.path);
+      task.blob = data;
+    })
+    .on('aborted', () => {
+      console.error('Request aborted by the user');
+    })
+    .on('error', (err) => {
+      console.error('Error', err);
+      throw err;
+    })
+    .on('end', async () => {
+      const { id } = await getConnection()
+        .getRepository(Task)
+        .save(task);
+
+      // const worker = workerFarm(
+      //   require.resolve('../../lib/workers/coverage-processor'));
+      // worker(coverageId, function (err, outp) {
+      //   if (err) {
+      //     console.log("error");
+      //     console.log(err);
+      //   }
+      //   console.log(outp)
+      //   workerFarm.end(worker)
+      // });
+      console.log(fields);
+      res.json({id: id});
+    });
 
 }));
 

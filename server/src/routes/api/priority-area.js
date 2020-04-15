1fb226cd6a07da815b3f1c14592cf8a4172bd588
Lachlan Hurst <lachlan.hurst@gmail.com>
2020-04-15 19:14:54 +1000

    upload handler triggers processing task. Added new handlers to get thumbnail images and PA data

--- server/src/routes/api/priority-area.js
+++ server/src/routes/api/priority-area.js
@@ -10,9 +10,58 @@ import { getConnection } from 'typeorm';
 
 import { asyncMiddleware, isAuthenticated } from '../utils';
 import { PriorityArea } from '../../lib/entity/priority-area';
+import { PriorityAreaSubmission } from '../../lib/entity/priority-area-submission';
 import { Task } from '../../lib/entity/task';
 
 const router = express.Router();
+
+
+// Gets a single priority by id
+router.get(
+  '/:id',
+  [isAuthenticated],
+  asyncMiddleware(async function (req, res)
+{
+  const id = req.params.id;
+
+  let pa = await getConnection()
+    .getRepository(PriorityArea)
+    .findOne(id);
+
+  if (_.isNil(pa)) {
+    let err = Boom.notFound(`Priority Area ${id} does not exist`);
+    throw err;
+  }
+
+  return res.json(pa);
+}));
+
+router.get(
+  '/:id/thumbnail',
+  [isAuthenticated],
+  asyncMiddleware(async function (req, res)
+{
+  const id = req.params.id;
+
+  let pa = await getConnection()
+    .getRepository(PriorityArea)
+    .findOne(
+      id,
+      { select: ["thumbnail"] }
+    );
+
+  if (_.isNil(pa)) {
+    let err = Boom.notFound(`No priority area found with id ${id}`);
+    throw err;
+  }
+
+  res.set('Content-length', pa.thumbnail.length);
+  res.set('Content-Type', 'image/png');
+
+  const readStream = new stream.PassThrough();
+  readStream.end(pa.thumbnail);
+  readStream.pipe(res);
+}));
 
 
 //upload for a new template, form includes fields that are needed by DB
@@ -21,8 +70,6 @@ router.put(
   [isAuthenticated],
   asyncMiddleware(async function (req, res)
 {
-  console.log("upload");
-
   let task = new Task();
   let fields = {};
 
@@ -43,22 +90,44 @@ router.put(
       throw err;
     })
     .on('end', async () => {
-      const { id } = await getConnection()
-        .getRepository(Task)
-        .save(task);
+      if (!_.has(fields, 'priorityAreaSubmissionId')) {
+        let err = Boom.badRequest(
+          "No `priorityAreaSubmissionId` given on form data");
+        throw err;
+      }
 
-      // const worker = workerFarm(
-      //   require.resolve('../../lib/workers/coverage-processor'));
-      // worker(coverageId, function (err, outp) {
-      //   if (err) {
-      //     console.log("error");
-      //     console.log(err);
-      //   }
-      //   console.log(outp)
-      //   workerFarm.end(worker)
-      // });
+      let pasId = fields.priorityAreaSubmissionId;
+      let taskId = undefined;
+
+      await getConnection().transaction(async transactionalEntityManager => {
+        const { id } = await getConnection()
+          .getRepository(Task)
+          .save(task);
+
+        let pasQuery = await getConnection()
+          .createQueryBuilder()
+          .update(PriorityAreaSubmission)
+          .set({ uploadTask: task })
+          .where("id = :id", { id: pasId })
+          .execute();
+
+        taskId = id;
+      });
+
+      const worker = workerFarm(
+        {maxRetries: 3},
+        require.resolve('../../lib/workers/priority-area-processor')
+      );
+      worker(taskId, function (err, outp) {
+        if (err) {
+          console.log("error");
+          console.log(err);
+        }
+        console.log(outp);
+        workerFarm.end(worker);
+      });
       console.log(fields);
-      res.json({id: id});
+      res.json({taskId: taskId});
     });
 
 }));

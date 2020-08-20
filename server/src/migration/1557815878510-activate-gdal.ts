a0adccce146546db62694a2a6ed96a2469a9e7c4
ecole <ek@fastlane-it.com>
2020-08-21 09:35:33 +1000

    Added RDS_MODE env var to skip system-level DB parameters configuration not available in RDS Postgres

--- server/src/migration/1557815878510-activate-gdal.ts
+++ server/src/migration/1557815878510-activate-gdal.ts
@@ -2,6 +2,7 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class activateGdal1557815878510 implements MigrationInterface {
 
+
     public async up(queryRunner: QueryRunner): Promise<any> {
       // The reason for catching errors generated here is that "rds_superuser"
       // role which is given to RDS admin user is not full-on postgres root
@@ -9,15 +10,15 @@ export class activateGdal1557815878510 implements MigrationInterface {
       // managed database instances securely and efficiently.
 
       // The enabled drivers within RDS alread include those listed below
+      const isRdsMode = process.env.RDS_MODE || false
 
-      try {
-        await queryRunner.query(`SET postgis.gdal_enabled_drivers = 'ENABLE_ALL'`);
-        await queryRunner.query(`ALTER DATABASE "postgres" SET postgis.gdal_enabled_drivers TO 'GTiff PNG JPEG'`);
-      } catch (error) {
-        console.log("Migration failed");
-        console.log("Note: this migration is expected to fail when run with an RDS database");
-        console.log(error);
+      if (isRdsMode) {
+          console.log("Running in RDS mode - skipping system-level DB parameters migration")
+      } else {
+          await queryRunner.query(`SET postgis.gdal_enabled_drivers = 'ENABLE_ALL'`);
+          await queryRunner.query(`ALTER DATABASE "postgres" SET postgis.gdal_enabled_drivers TO 'GTiff PNG JPEG'`);
       }
+
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {

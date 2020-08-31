dd560545e5582bc79ced62c55dbfba9bf4183412
Lachlan Hurst <lachlan.hurst@gmail.com>
2020-08-31 15:57:17 +1000

    load auth server details from SCT server instead of env vars
    these env vars were being passed into the client container at build time which was causing some issues in a new environment
    
    Generally this has been a pain to maintain, so a good fix to have implemented

--- client/src/boot/authenticate.js
+++ client/src/boot/authenticate.js
@@ -1,17 +1,23 @@
 import VueAuthenticate from 'vue-authenticate';
+import axios from 'axios';
 
-const authServer = process.env.AUTH_HOST
-const clientId = process.env.AUTH_CLIENT_ID
+export default async ({ app, router, store, Vue }) => {
 
-export default ({ app, router, store, Vue }) => {
+  // fetch details of the authentication server from the SCT backend
+  // we do this to avoid having environment variables built into the client
+  // these env vars were applied at build time making them hard to work with
+  // in some deployment environments
+  const authConfigResponse = await axios.get('/api/util/auth-details');
+  const authConfig = authConfigResponse.data;
+
   Vue.use(VueAuthenticate, {
     providers: {
       crcsi: {
-        clientId: clientId,
-        redirectUri: window.location.origin + '/auth/callback', // Your client app URL
+        clientId: authConfig.authClientId,
+        redirectUri: window.location.origin + '/auth/callback',
         name: 'crcsi',
         url: '/api/auth/crcsi',
-        authorizationEndpoint: authServer + '/oauth2/authorize',
+        authorizationEndpoint: authConfig.authHost + '/oauth2/authorize',
         requiredUrlParams: ['scope', 'state'],
         scope: ['profile', 'email'],
         scopePrefix: 'openid',
